92406ca85eac3190b36cfe8693c548429ccee285
Josh Turgeon <j_turgeon88@yahoo.com>
2018-03-06 13:50:58 -0500

    Todos Part 2: Phase 2 API Utils complete/tested and thunk middleware complete/tested (not action creators)

--- todos/frontend/actions/todo_actions.js
+++ todos/frontend/actions/todo_actions.js
@@ -1,3 +1,5 @@
+import * as TodoAPIUtil from '../util/todo_api_util';
+
 export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
 
@@ -10,3 +12,8 @@ export const receiveTodo = todo => ({
   type: RECEIVE_TODO,
   todo
 });
+
+
+export const fetchTodos = () => dispatch (
+  TodoAPIUtil.fetchTodos().then(todos => dispatch(receiveTodos(todos)))
+);
--- todos/frontend/store/store.js
+++ todos/frontend/store/store.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from '../middleware/thunk'
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
   return store;
 }
 
+window.configureStore = configureStore; 
 
 export default configureStore;
