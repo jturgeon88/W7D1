dc57d472ade11fac75cdf7cd07315a6a1783f204
Josh Turgeon <j_turgeon88@yahoo.com>
2018-03-08 10:33:48 -0500

    Phse 5 complete (no updateTodo tho)

--- todos_frontend/frontend/components/todos/todo_list_item.jsx
+++ todos_frontend/frontend/components/todos/todo_list_item.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import TodoList from './todo_list'
 
+const toggleDone = (todo, receiveTodo) => {
+  todo.done = !todo.done;
+  receiveTodo(todo);
+};
+
+const doneButtonTextifier = (todo) => {
+  return todo.done ? "Undo" : "Done";
+};
+
 class TodoListItem extends React.Component {
+
+
   render () {
-    const { todo, removeTodo } = this.props;
+    const { todo, receiveTodo, removeTodo } = this.props;
     const title = this.props.todo.title;
     return (
       <div>
         <li>
           {title}
         </li>
+        <button onClick={() => toggleDone(todo, receiveTodo)}>
+          {doneButtonTextifier(todo)}
+        </button>
         <button
           className="delete-button"
           onClick={() => removeTodo(todo)}>Delete Todo</button>
